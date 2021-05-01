1b21bb294c63ee83068c4708c6b2fd5d0e37fca7
Travis Smith <travis.smith@weather.com>
2021-05-01 11:54:13 -0400

    Refactor with FileLoader

--- src/TemplateLoader.php
+++ src/TemplateLoader.php
@@ -35,52 +35,7 @@ if ( ! class_exists( __NAMESPACE__ . '\TemplateLoader' ) ) {
 	 *
 	 * @package WPS\WP
 	 */
-	class TemplateLoader {
-
-		/**
-		 * Prefix for filter names.
-		 *
-		 * An _ will be added at the end,
-		 *
-		 * @example 'plugin_prefix'.
-		 * @var string
-		 */
-		protected $prefix = 'yourplugin';
-
-		/**
-		 * Directory name where custom templates for this plugin should be found in the theme.
-		 *
-		 * @example 'plugin-templates'.
-		 * @var string
-		 */
-		protected $theme_template_directory = 'templates';
-
-		/**
-		 * Reference to the root directory path of this plugin.
-		 *
-		 * Can either be a defined constant, or a relative reference from where the subclass lives.
-		 *
-		 * @example YOUR_PLUGIN_TEMPLATE or plugin_dir_path( dirname( __FILE__ ) ); etc.
-		 * @var string
-		 */
-		protected $plugin_directory = 'YOUR_PLUGIN_DIR';
-
-		/**
-		 * Reference to the template directory path of this plugin.
-		 *
-		 * Can either be a defined constant, or a relative reference from where the subclass lives.
-		 *
-		 * @example 'templates' or 'includes/templates', etc.
-		 * @var string
-		 */
-		protected $templates_directory = 'templates';
-
-		/**
-		 * Internal use only: Store located template paths.
-		 *
-		 * @var array
-		 */
-		private $template_path_cache = [];
+	class TemplateLoader extends FileLoader {
 
 		/**
 		 * Internal use only: Store variable names used for template data.
@@ -92,37 +47,6 @@ if ( ! class_exists( __NAMESPACE__ . '\TemplateLoader' ) ) {
 		 * @var array
 		 */
 		private $template_data_var_names = [];
-
-		/**
-		 * TemplateLoader constructor.
-		 *
-		 * @param array $args Loader Args.
-		 */
-		public function __construct( $args = [] ) {
-
-			if ( ! empty( $args ) ) {
-
-				// Backwards compatibility.
-				if ( isset( $args['filter_prefix'] ) ) {
-					$args['prefix'] = $args['filter_prefix'];
-					unset( $args['filter_prefix'] );
-				}
-
-				$defaults = [
-					'prefix'                   => 'yourplugin',
-					'theme_template_directory' => 'templates',
-					'plugin_directory'         => 'plugin-templates',
-					'templates_directory'      => 'templates',
-				];
-
-				$args = wp_parse_args( $args, $defaults );
-
-				foreach ( $args as $var => $val ) {
-					$this->{$var} = $val;
-				}
-			}
-
-		}
 
 		/**
 		 * Clean up template data.
@@ -193,9 +117,7 @@ if ( ! class_exists( __NAMESPACE__ . '\TemplateLoader' ) ) {
 		 *
 		 */
 		public function load_template_part( $slug, $name = null ) {
-
-			return $this->get_template_part( $slug, $name, true );
-
+			return $this->load( $slug, $name );
 		}
 
 		/**
@@ -211,51 +133,11 @@ if ( ! class_exists( __NAMESPACE__ . '\TemplateLoader' ) ) {
 		 *
 		 */
 		public function get_template_part( $slug, $name = null, $load = false ) {
-			// Execute code for this part.
-			do_action( 'get_template_part_' . $slug, $slug, $name );
-			do_action( $this->get_prefix() . '_get_template_part_' . $slug, $slug, $name );
-
-			// Get files names of templates, for given slug and name.
-			$templates = $this->get_template_file_names( $slug, $name );
-
-			// Return the part that is found.
-			return $this->locate_template( $templates, $load, false );
-		}
-
-		/**
-		 * Given a slug and optional name, create the file names of templates.
-		 *
-		 * Templates will be:
-		 *  - $slug-$name.php
-		 *  - $name.php
-		 *  - $slug.php
-		 *
-		 * @param string $slug Template slug.
-		 * @param string $name Template name.
-		 *
-		 * @return array
-		 */
-		protected function get_template_file_names( $slug, $name ) {
-			$templates = [];
-			if ( isset( $name ) ) {
-				$templates[] = $slug . '-' . $name . '.php';
+			if ($load) {
+				return $this->load( $slug, $name );
 			}
-			$templates[] = $name . '.php';
-			$templates[] = $slug . '.php';
 
-			/**
-			 * Allow template choices to be filtered.
-			 *
-			 * The resulting array should be in the order of most specific first, to least specific last.
-			 * e.g. 0 => recipe-instructions.php, 1 => recipe.php
-			 *
-			 * @param array $templates Names of template files that should be looked for, for given slug and name.
-			 * @param string $slug Template slug.
-			 * @param string $name Template name.
-			 *
-			 * @since 0.0.1
-			 */
-			return apply_filters( $this->get_prefix() . '_get_template_part', $templates, $slug, $name );
+			return $this->get( $slug, $name );
 		}
 
 		/**
@@ -275,116 +157,11 @@ if ( ! class_exists( __NAMESPACE__ . '\TemplateLoader' ) ) {
 		 *
 		 */
 		public function locate_template( $template_names, $load = false, $require_once = true ) {
-
-			// Use $template_names as a cache key - either first element of array or the variable itself if it's a string.
-			$cache_key = is_array( $template_names ) ? $template_names[0] : $template_names;
-
-			// If the key is in the cache array, we've already located this file.
-			if ( isset( $this->template_path_cache[ $cache_key ] ) ) {
-				$located = $this->template_path_cache[ $cache_key ];
-			} else {
-
-				// No file found yet.
-				$located = false;
-
-				// Remove empty entries.
-				$template_names = array_filter( (array) $template_names );
-				$template_paths = $this->get_template_paths();
-
-				// Try to find a template file.
-				foreach ( $template_names as $template_name ) {
-					// Trim off any slashes from the template name & replace any spaces with dashes.
-					$template_name = str_replace( ' ', '-', ltrim( $template_name, '/' ) );
-
-					// Try locating this template file by looping through the template paths.
-					foreach ( $template_paths as $template_path ) {
-						if ( file_exists( $template_path . $template_name ) && is_file( $template_path . $template_name ) ) {
-							$located = $template_path . $template_name;
-
-							// Store the template path in the cache.
-							$this->template_path_cache[ $cache_key ] = $located;
-							break 2;
-						}
-					}
-				}
-
+			$located = $this->locate( $template_names );
+			if ( $located & $load ) {
+				\load_template( $located, $require_once );
 			}
-
-			if ( $load && $located ) {
-				load_template( $located, $require_once );
-			}
-
 			return $located;
 		}
-
-		/**
-		 * Return a list of paths to check for template locations.
-		 *
-		 * Default is to check in a child theme (if relevant) before a parent theme, so that themes which inherit from a
-		 * parent theme can just overload one file. If the template is not found in either of those, it looks in the
-		 * theme-compat folder last.
-		 *
-		 * @return mixed
-		 */
-		protected function get_template_paths() {
-			$theme_directory = trailingslashit( $this->theme_template_directory );
-
-			$file_paths = [
-				10  => trailingslashit( get_template_directory() ) . $theme_directory,
-				100 => $this->get_templates_dir(),
-			];
-
-			// Only add this conditionally, so non-child themes don't redundantly check active theme twice.
-			if ( $this->is_child_theme() ) {
-				$file_paths[1] = trailingslashit( get_stylesheet_directory() ) . $theme_directory;
-			}
-
-			/**
-			 * Allow ordered list of template paths to be amended.
-			 *
-			 * @param array $var Default is directory in child theme at index 1, parent theme at 10, and plugin at 100.
-			 *
-			 * @since 0.0.1
-			 *
-			 */
-			$file_paths = apply_filters( $this->get_prefix() . '_template_paths', $file_paths );
-
-			// sort the file paths based on priority.
-			ksort( $file_paths, SORT_NUMERIC );
-
-			return array_map( 'trailingslashit', $file_paths );
-		}
-
-		/**
-		 * Whether a child theme is in use.
-		 *
-		 * @return bool true if a child theme is in use, false otherwise.
-		 **/
-		protected function is_child_theme() {
-
-			return ( get_template_directory() !== get_stylesheet_directory() );
-
-		}
-
-		/**
-		 * Return the path to the templates directory in this plugin.
-		 *
-		 * @return string
-		 */
-		protected function get_templates_dir() {
-
-			return trailingslashit( $this->plugin_directory ) . $this->templates_directory;
-
-		}
-
-		/**
-		 * Gets the sanitized prefix.
-		 *
-		 * @return string
-		 */
-		private function get_prefix() {
-			return str_replace( '-', '_', $this->prefix );
-		}
-
 	}
 }
